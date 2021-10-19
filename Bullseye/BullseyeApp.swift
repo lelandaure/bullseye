b98c8d680f41bda53103603288f2fe02cb9cefae
lel <lelandaure01@gmail.com>
2021-10-18 23:43:10 -0500

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Luis Enrique Landaure Vargas on 18/10/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
